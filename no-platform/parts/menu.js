c08e48b29bb76673c92a004a931ea6a90f9d337f
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2017-07-13 16:35:38 +0200

    no-platform: styles met scope

--- no-platform/parts/menu.js
+++ no-platform/parts/menu.js
@@ -4,5 +4,5 @@ var $ = require("jquery");
 
 window.menu = {}
 window.menu.Init = function (v) {
-  $('#' + v.id + ' [href="' + v.current + '"]').parent().addClass('current');
+  $('#nav [href="' + v.current + '"]').parent().addClass('current');
 }
