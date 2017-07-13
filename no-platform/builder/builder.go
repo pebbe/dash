750ad8cc1ec265ad5d524f7965865c405a5772bc
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2017-07-13 14:33:30 +0200

    no-platform: eerste werkende versie

--- no-platform/builder/builder.go
+++ no-platform/builder/builder.go
@@ -3,27 +3,32 @@ package main
 import (
 	"github.com/pebbe/util"
 
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
 )
 
 var (
-	basedir = ".."
+	rePart = regexp.MustCompile(`<!--\[\[([a-zA-Z0-9]+)(\s*)(.*?)\]\]-->`)
+	reVar  = regexp.MustCompile(`{{[a-zA-Z0-9]+}}`)
+
+	basedir string
 
 	x = util.CheckErr
+
+	jsfiles = make(map[string]bool)
 )
 
 func main() {
 
-	x(os.RemoveAll(filepath.Join(basedir, "work")))
-	for _, dirname := range []string{"style", "html", "script"} {
-		x(os.MkdirAll(filepath.Join(basedir, "work", dirname), 0777))
-	}
+	basedir = os.Args[1]
 
-	fp, err := os.Create(filepath.Join(basedir, "work", "script", "script.src.js"))
+	fp, err := os.Create(filepath.Join(basedir, "work", "script.src.js"))
 	x(err)
 	for _, dirname := range []string{"pages", "parts"} {
 		p := filepath.Join(basedir, dirname)
@@ -33,9 +38,83 @@ func main() {
 			filename := fi.Name()
 			if strings.HasSuffix(filename, ".js") {
 				base := filename[:len(filename)-3]
-				fmt.Fprintf(fp, "var %s = require(\"%s\")\n", base, filepath.Join("..", "..", dirname, base))
+				jsfiles[base] = true
+				fmt.Fprintf(fp, "var %s = require(\"%s\")\n", base, filepath.Join(p, base))
 			}
 		}
 	}
 	fp.Close()
+
+	p := filepath.Join(basedir, "pages")
+	fis, err := ioutil.ReadDir(p)
+	x(err)
+	for _, fi := range fis {
+		name := fi.Name()
+		if strings.HasSuffix(name, ".html") {
+			doPage(name[:len(name)-5])
+		}
+	}
+}
+
+func doPage(page string) {
+	filename := filepath.Join(basedir, "pages", page+".html")
+	b, err := ioutil.ReadFile(filename)
+	x(err)
+	html := string(b)
+
+	pagefiles := make(map[string]string)
+
+	mm := rePart.FindAllStringSubmatch(html, -1)
+	if mm != nil {
+		for _, m := range mm {
+			if jsfiles[m[1]] {
+				pagefiles[m[1]] = m[3]
+			}
+		}
+	}
+
+	var buffer bytes.Buffer
+	for key, value := range pagefiles {
+		fmt.Fprintf(&buffer, "%s.Init(%s);\n", key, value)
+	}
+	i := strings.LastIndex(html, "</body>")
+	html = html[:i] + `
+<script src="script.js"></script>
+<script>
+` + buffer.String() + `
+</script>
+` + html[i:]
+
+	html = rePart.ReplaceAllStringFunc(html, doPart)
+
+	fp, err := os.Create(filepath.Join(basedir, "dist", page+".html"))
+	x(err)
+	fp.Write([]byte(html))
+	fp.Close()
+}
+
+func doPart(s string) string {
+	m := rePart.FindStringSubmatch(s)
+
+	translate := make(map[string]string)
+	if m[3] != "" {
+		var v map[string]interface{}
+		x(json.Unmarshal([]byte(m[3]), &v))
+		for key, val := range v {
+			translate[key] = fmt.Sprint(val)
+		}
+	}
+
+	filename := filepath.Join(basedir, "parts", m[1]+".html")
+	b, err := ioutil.ReadFile(filename)
+	x(err)
+
+	html := string(b)
+
+	html = reVar.ReplaceAllStringFunc(html,
+		func(s string) string {
+			return translate[s[2:len(s)-2]]
+		})
+
+	return html
 }
