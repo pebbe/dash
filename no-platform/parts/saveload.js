8f52da4c999f80525acd09d8b563d4070ecc9d18
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2017-07-14 11:22:38 +0200

    no-platform: save/load

---
+++ no-platform/parts/saveload.js
@@ -0,0 +1,38 @@
+'use strict'
+
+var $ = require("jquery");
+
+var saveload = {}
+
+saveload.data = {}
+
+saveload.Init = function (v) {
+  saveload.data[v.id] = {}
+  var data = saveload.data[v.id]
+  data.text = $('#' + v.id + ' input')
+  data.output = $('#' + v.id + ' .loaded')
+}
+
+saveload.save = function (id) {
+  var data = saveload.data[id]
+  var text = data.text.val()
+  data.text.val('')
+  data.output.text('')
+  $.post('service/save', text, function (resp) {
+    data.output.text(resp)
+  }).fail(function (e) {
+    data.output.text(e['status'] + ' ' + e['statusText'])
+  })
+}
+
+saveload.load = function (id) {
+  var data = saveload.data[id]
+  data.output.text('')
+  $.get('service/load', function (resp) {
+    data.output.text(resp)
+  }).fail(function (e) {
+    data.output.text(e['status'] + ' ' + e['statusText'])
+  })
+}
+
+window.saveload = saveload
