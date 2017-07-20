7fd53fc0cf5657ae05214465fd36a83ee8a9e278
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2017-07-20 13:28:07 +0200

    no-platform: twimagelist: css, html, js

--- no-platform/src/parts/twimagelist.js
+++ no-platform/src/parts/twimagelist.js
@@ -23,16 +23,14 @@ twimagelist.create = function (id, n, image) {
   var o = $('#' + id + " .template").first().clone()
   o.attr("id", id + "-" + n)
   o.removeClass("template")
-  $(o).find("a").attr("href", image.link)
+  $(o).attr("href", image.link)
   $(o).find("img").attr("src", image.img)
   o.appendTo("#" + id)
 }
 
 twimagelist.error = function (id, e) {
-  var o = $('#' + id + " .template").first().clone()
-  o.removeClass("template")
-  $(o).find(".error").text(e.status + " " + e.statusText)
-  o.appendTo("#" + id)
+  var err = document.createElement("div");
+  $(err).addClass("error").text(e.status + " " + e.statusText).appendTo("#" + id)
 }
 
 window.twimagelist = twimagelist
