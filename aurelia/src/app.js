24785cbbd2e888a915750c731627cf442ece4a34
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2017-08-03 15:27:35 +0200

    aurelia: zoeken: searchText & debounce

--- aurelia/src/app.js
+++ aurelia/src/app.js
@@ -2,7 +2,6 @@ export class App {
     configureRouter(config, router) {
         this.router = router;
         config.title = "Aurelia | Dash";
-        // TODO: waarvoor dienen 'name' en 'nav'?
         config.map([
             { route: ["", "home"], name: "home", moduleId: "home", nav: true, title: "Home" },
             { route: "page2", name: "page2", moduleId: "page-two", nav: true, title: "Page 2" }
