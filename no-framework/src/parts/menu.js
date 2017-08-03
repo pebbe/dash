b77a16e79e0be7ab371051a69a4f00e5b30e21d9
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2017-08-03 15:27:02 +0200

    no-framework: auth

--- no-framework/src/parts/menu.js
+++ no-framework/src/parts/menu.js
@@ -4,12 +4,60 @@ var $ = require("jquery");
 
 var menu = {}
 
+menu.user_id = ''
+
+menu.need_auth = ['user.html']
+
 /*
  * parameters:
  *   - current
  */
 menu.Init = function (v) {
   $('#nav [href="' + v.current + '"]').parent().addClass('current');
+
+  $('#login button').on('click', function (ev) {
+    menu.login(v)
+  })
+
+  $('#logout button').on('click', function (ev) {
+    menu.logout(v)
+  })
+
+  menu.user_id = localStorage.getItem('user_id') || ''
+  menu.setAuth(v)
+}
+
+menu.login = function (v) {
+  menu.user_id = $('#login input').val().trim()
+  $('#login input').val('')
+  localStorage.setItem('user_id', menu.user_id)
+  menu.setAuth(v)
+}
+
+menu.logout = function (v) {
+  menu.user_id = ''
+  localStorage.setItem('user_id', '')
+  menu.setAuth(v)
+}
+
+menu.setAuth = function (v) {
+  if (menu.user_id) {
+    $('#login').addClass('hidden')
+    $('#logout').removeClass('hidden')
+    for (var i = 0; i < menu.need_auth.length; i++) {
+      $('nav div.left a[href="' + menu.need_auth[i] + '"]').parent().removeClass('hidden')
+    }
+  } else {
+    if (menu.need_auth.indexOf(v.current) >= 0) {
+      document.location = '.'
+      return
+    }
+    $('#login').removeClass('hidden')
+    $('#logout').addClass('hidden')
+    for (var i = 0; i < menu.need_auth.length; i++) {
+      $('nav div.left a[href="' + menu.need_auth[i] + '"]').parent().addClass('hidden')
+    }
+  }
 }
 
 window.app = window.app || {}
---
+++ no-framework/src/parts/userpage.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var $ = require("jquery");
+
+var userpage = {}
+
+/*
+ * parameters: geen
+ */
+userpage.Init = function () {
+    $(".userpage h1").text("~(Hello)~, " + localStorage.getItem('user_id') || '')
+}
+
+window.app = window.app || {}
+window.app.userpage = userpage
