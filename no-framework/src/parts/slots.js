79b2d12f4bf5c1dab84a5ed94fdbe8b53380a7d7
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2017-08-09 11:46:37 +0200

    no-framework: slots

---
+++ no-framework/src/parts/slots.js
@@ -0,0 +1,23 @@
+'use strict'
+
+var $ = require("jquery");
+
+var slots = {}
+
+/*
+ * parameters:
+ *   - id
+ *   - slots
+ */
+slots.Init = function (v) {
+    for (var i = 0; i < v.slots.length; i++) {
+        var dst = $('#' + v.id + ' .' + v.slots[i][0])
+        var src = $(v.slots[i][1])
+        dst.html(src.html())
+        src.remove()
+    }
+    $('#' + v.id + '>div>div').removeClass('hidden')
+}
+
+window.app = window.app || {}
+window.app.slots = slots
