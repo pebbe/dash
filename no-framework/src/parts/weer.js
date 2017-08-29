1039e6ec171eaf08c02404f83adab9b3f40bf485
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2017-08-29 12:00:28 +0200

    no-framework: weer: foutafhandeling

--- no-framework/src/parts/weer.js
+++ no-framework/src/parts/weer.js
@@ -12,18 +12,18 @@ weer.Init = function (v) {
     $.get('http://www.let.rug.nl/kleiweg/bin/cdf.sh', function (resp) {
         // returned data will be an XML root element, text string, JavaScript
         // file, or JSON object, depending on the MIME type of the response
-        var data;
+
         if (typeof (resp) == 'string') {
-            try {
-                data = JSON.parse(resp);
-            } catch (e) {
-                weer.error(v.id, e);
-                return;
-            }
-        } else {
-            data = resp;
+            weer.error(v.id, '~(Error)~', resp);
+            return;
         }
-        var names = data.variables.stationname.values;
+
+        if (!(resp.variables && resp.variables.stationname)) {
+            weer.error(v.id, '~(failed to parse JSON object)~');
+            return;
+        }
+        var vars = resp.variables;
+        var names = vars.stationname.values;
         var idx = 0;
         var patt = /groningen/i;
         for (idx = 0; idx < names.length; idx++) {
@@ -36,22 +36,27 @@ weer.Init = function (v) {
             return;
         }
         $('#' + v.id + ' .plaats').text('~(The weather in)~ ' + names[idx]);
-        var t1 = data.variables.tn.values[idx];
-        var t2 = data.variables.tx.values[idx];
+        var t1 = vars.tn.values[idx];
+        var t2 = vars.tx.values[idx];
         if (t1 > -300 && t2 > -300) {
             $('#' + v.id + ' .temp').text(((t1 + t2) / 2).toFixed(1) + '°C');
         } else {
             $('#' + v.id + ' .temp').text('??°C');
         }
-        var date = new Date(1000 * (data.variables.time.values[0] - 631152000));
+        var date = new Date(1000 * (vars.time.values[0] - 631152000));
         $('#' + v.id + ' .tijd').text(date);
-    }).fail(function (e) {
-        weer.error(v.id, e);
+    }).fail(function (resp) {
+        if (resp.status == 200) {
+            weer.error(v.id, '~(invalid response)~', resp.responseText);
+        } else {
+            weer.error(v.id, resp.status + ' ' + resp.statusText);
+        }
     });
 };
 
-weer.error = function (id, err) {
-    $('#' + id + ' plaats').text(err);
+weer.error = function (id, err, msg) {
+    $('#' + id + ' .plaats').text(err);
+    $('#' + id + ' .tijd').text(msg);
 };
 
 window.app = window.app || {};
