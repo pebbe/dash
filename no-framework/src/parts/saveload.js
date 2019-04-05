04f99ae6a0cf2d6ae88905ec9903ffe0fd38f1a2
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2019-04-05 12:03:58 +0200

    Terug naar een normale webserver

--- no-framework/src/parts/saveload.js
+++ no-framework/src/parts/saveload.js
@@ -9,18 +9,17 @@ saveload.data = {};
 /*
  * parameters:
  *   - id
- *   - bin
  */
 saveload.Init = function (v) {
 
     // eslint-disable-next-line no-unused-vars
     $('#' + v.id + ' .save').on('click', function (ev) {
-        saveload.save(v.id, v.bin);
+        saveload.save(v.id);
     });
 
     // eslint-disable-next-line no-unused-vars
     $('#' + v.id + ' .load').on('click', function (ev) {
-        saveload.load(v.id, v.bin);
+        saveload.load(v.id);
     });
 
     saveload.data[v.id] = {};
@@ -29,22 +28,22 @@ saveload.Init = function (v) {
     data.output = $('#' + v.id + ' .loaded');
 };
 
-saveload.save = function (id, bin) {
+saveload.save = function (id) {
     var data = saveload.data[id];
     var text = data.text.val();
     data.text.val('');
     data.output.text('');
-    $.post(bin + '/save.~(LANG)~', text, function (resp) {
+    $.post('../bin/save.~(LANG)~', text, function (resp) {
         data.output.text(resp);
     }).fail(function (e) {
         data.output.text(e['status'] + ' ' + e['statusText']);
     });
 };
 
-saveload.load = function (id, bin) {
+saveload.load = function (id) {
     var data = saveload.data[id];
     data.output.text('');
-    $.get(bin + '/load', function (resp) {
+    $.get('../bin/load', function (resp) {
         data.output.text(resp);
     }).fail(function (e) {
         data.output.text(e['status'] + ' ' + e['statusText']);
